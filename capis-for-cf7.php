f46686c9a7015772006d5345a1c3b7f74e4b8566
Eric Chuang <huanyi.chuang@gmail.com>
2023-05-25 16:24:31 +0800

    First version

--- capis-for-cf7.php
+++ capis-for-cf7.php
@@ -36,47 +36,159 @@ if ( ! defined( 'WPINC' ) ) {
  * Rename this for your plugin and update it as you release new versions.
  */
 define( 'CAPIS_FOR_CF7_VERSION', '1.0.0' );
+define( 'CAPIS_PIXEL_ID', '137567753520385' );
 
-/**
- * The code that runs during plugin activation.
- * This action is documented in includes/class-capis-for-cf7-activator.php
- */
-function activate_capis_for_cf7() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-capis-for-cf7-activator.php';
-	Capis_For_Cf7_Activator::activate();
+function cf7_facebook_pixel_settings_page() {
+    ?>
+    <div class="wrap">
+        <h2>Facebook Pixel</h2>
+        <form method="post" action="options.php">
+            <?php
+            settings_fields('cf7_facebook_pixel_settings');
+            do_settings_sections('cf7_facebook_pixel');
+            submit_button();
+            ?>
+        </form>
+    </div>
+    <?php
 }
 
-/**
- * The code that runs during plugin deactivation.
- * This action is documented in includes/class-capis-for-cf7-deactivator.php
- */
-function deactivate_capis_for_cf7() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-capis-for-cf7-deactivator.php';
-	Capis_For_Cf7_Deactivator::deactivate();
+function cf7_facebook_pixel_settings_init() {
+    add_settings_section(
+        'cf7_facebook_pixel_section',
+        '',
+        '',
+        'cf7_facebook_pixel'
+    );
+
+    add_settings_field(
+        'cf7_facebook_pixel_api_token',
+        'API Token',
+        'cf7_facebook_pixel_api_token_callback',
+        'cf7_facebook_pixel',
+        'cf7_facebook_pixel_section'
+    );
+
+	add_settings_field(
+        'cf7_facebook_pixel_id',
+        'Pixel ID',
+        'cf7_facebook_pixel_id_callback',
+        'cf7_facebook_pixel',
+        'cf7_facebook_pixel_section'
+    );
+
+	add_settings_field(
+        'cf7_facebook_test_event',
+        'Test event',
+        'cf7_facebook_test_event_callback',
+        'cf7_facebook_pixel',
+        'cf7_facebook_pixel_section'
+    );
+
+    register_setting(
+        'cf7_facebook_pixel_settings',
+        'cf7_facebook_pixel_api_token'
+    );
+
+	register_setting(
+        'cf7_facebook_pixel_settings',
+        'cf7_facebook_pixel_id'
+    );
+
+	register_setting(
+        'cf7_facebook_pixel_settings',
+        'cf7_facebook_test_event',
+    );
 }
 
-register_activation_hook( __FILE__, 'activate_capis_for_cf7' );
-register_deactivation_hook( __FILE__, 'deactivate_capis_for_cf7' );
-
-/**
- * The core plugin class that is used to define internationalization,
- * admin-specific hooks, and public-facing site hooks.
- */
-require plugin_dir_path( __FILE__ ) . 'includes/class-capis-for-cf7.php';
-
-/**
- * Begins execution of the plugin.
- *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
- *
- * @since    1.0.0
- */
-function run_capis_for_cf7() {
-
-	$plugin = new Capis_For_Cf7();
-	$plugin->run();
-
+function cf7_facebook_pixel_api_token_callback() {
+    $api_token = get_option('cf7_facebook_pixel_api_token');
+    ?>
+    <input type="text" name="cf7_facebook_pixel_api_token" value="<?php echo esc_attr($api_token); ?>" />
+    <?php
 }
-run_capis_for_cf7();
+
+function cf7_facebook_pixel_id_callback() {
+    $pixel_id = get_option('cf7_facebook_pixel_id');
+    ?>
+    <input type="text" name="cf7_facebook_pixel_id" value="<?php echo esc_attr($pixel_id); ?>" />
+    <?php
+}
+
+function cf7_facebook_test_event_callback() {
+    $test_event = get_option('cf7_facebook_test_event');
+    ?>
+    <input type="text" name="cf7_facebook_test_event" value="<?php echo esc_attr($test_event); ?>" />
+    <?php
+}
+
+add_action('admin_init', 'cf7_facebook_pixel_settings_init');
+add_action('admin_menu', 'cf7_facebook_pixel_add_options_page');
+
+function cf7_facebook_pixel_add_options_page() {
+    add_options_page(
+        'Facebook Pixel',
+        'Facebook Pixel',
+        'manage_options',
+        'cf7_facebook_pixel',
+        'cf7_facebook_pixel_settings_page'
+    );
+}
+add_action('wpcf7_mail_sent', 'cf7_facebook_pixel_send_conversion', 10, 1);
+
+function cf7_facebook_pixel_send_conversion($contact_form) {
+    $api_token = get_option('cf7_facebook_pixel_api_token');
+	$pixel_id = get_option('cf7_facebook_pixel_id');
+	$test_event = get_option('cf7_facebook_test_event');
+    $submission = WPCF7_Submission::get_instance();
+
+	if ($submission) {
+        $data = $submission->get_posted_data();
+
+        // 在這裡組織要傳送到 Facebook Pixel Conversions API 的資料
+
+        $url = 'https://graph.facebook.com/v13.0/' . $pixel_id . '/events';
+        $args = array(
+            'method'  => 'POST',
+            'headers' => array(
+                'Content-Type' => 'application/json',
+				// Detect current agent
+				'User-Agent' => $_SERVER['HTTP_USER_AGENT'],
+            ),
+            'body'    => json_encode(array(
+                'data' => array(
+                    array(
+                        'event_name' => 'lead',
+                        'event_time' => time(),
+                        'user_data'  => array(
+                            'em' => hash( 'sha256', $data['your-email'] ), // 根據表單欄位名稱更改
+                        ),
+                    ),
+                ),
+                'access_token' => $api_token,
+            )),
+        );
+
+		if ( ! empty( $test_event ) ) {
+			$args['test_event_code'] = $test_event; // 測試用，請移除
+		}
+
+        // 使用 cURL 或其他方式發送 POST 請求到 Facebook Pixel Conversions API
+        $response = wp_remote_post($url, $args);
+
+        if (!is_wp_error($response)) {
+            $response_code = wp_remote_retrieve_response_code($response);
+            if ($response_code === 200) {
+                // 成功回應處理
+				error_log( 'Facebook Pixel Conversions API Success: ' . $response_code );
+            } else {
+                // 錯誤回應處理
+				error_log( 'Facebook Pixel Conversions API Response Error: ' . $response_code );
+				error_log( 'Facebook Pixel Conversions API Response: ' . print_r( $response['body']['error']['error_user_msg'], true ) );
+            }
+        } else {
+            // 錯誤處理
+			error_log( 'Facebook Pixel Conversions API No Response: ' . $response->get_error_message() );
+        }
+    }
+}
